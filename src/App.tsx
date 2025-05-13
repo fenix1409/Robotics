1d41742ba56eadd029ef33f0d8efbb308820b3d1
fenix1409 <b2550486@gmail.com>
2025-05-13 15:08:25 +0500

    feat: add Button and Hero components
    - Created a Button component with basic structure.
    - Created a Hero section component with a container class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import ApplicationForm from './components/FormData'
 import Header from './sections/Header'
 
 function App() {
   return (
     <main>
       <Header />
-      <ApplicationForm/>
     </main>
   )
 }
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='container hero'>
+        <div className="flex items-center justify-between">
+            <div className='w-[651px] p-[50px] bg-white rounded-[30px]'>
+                <h1 className='text-[84px] leading-[100%] font-semibold mb-[9px]'>JustRobotics</h1>
+                <p className='text-[40px] leading-[42px] font-medium mb-[79px]'>твой проводник в мир технологий и искусства</p>
+                <span className='text-[20px] leading-[100%] mb-[9px]'>получите бесплатное первое занятие</span>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
